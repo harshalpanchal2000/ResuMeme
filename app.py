b65dce3f85e17de1d336ccb07d7993d0791d3fe1
Harshal Panchal <78840044+harshalpanchal2000@users.noreply.github.com>
2024-05-08 16:44:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 # Set page configuration
 st.set_page_config(
-    page_title="ResuMeme - Attract Employers, Not Glitches!",
+    page_title="ResuMeme",
     page_icon=":clipboard:",
-     initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded"
 )
 
 # Streamlit UI
@@ -29,10 +29,10 @@ if uploaded_file is not None:
     st.subheader("Resume Content")
     st.write(resume_text)
 
-    # Add attribution
+# Add attribution with left padding
 st.markdown(
-    """<div style="position: fixed; bottom: 10px; width: 100%; text-align: center; padding-left: 37px;">
-    Built by <a href="https://www.linkedin.com/in/harshal-panchal/" target="_blank">Harshal Panchal</a>
+    """<div style="position: fixed; bottom: 10px; width: 100%; text-align: center;">
+    <span style="padding-left: 20px;">Built by <a href="https://www.linkedin.com/in/harshal-panchal/" target="_blank">Harshal Panchal</a></span>
     </div>""",
     unsafe_allow_html=True
 )
